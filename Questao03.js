ff136e5ac715889fb8b689bdcba6e8cd8538cbfc
Carolina de Faria Coutinho <75336624+CarolinaFCoutinho@users.noreply.github.com>
2021-10-26 21:15:04 +0000

    Initial commit

---
+++ Questao03.js
@@ -0,0 +1,12 @@
+var Aluno = "João da Silva";
+var nota01 = 7;
+var nota02 = 8.5;
+var valor01 = nota01 * 0.6;
+var valor02 = nota02 * 0.4;
+var soma = valor01 + valor02;
+
+alert("Nome do aluno: "+ Aluno +"\n"+ 
+"Nota 1: " + nota01 +"\n"+"Nota 2: " + nota02);
+
+alert("\nNome do aluno: " + Aluno +"\n"+ 
+	  "Nota final: "+ soma);
